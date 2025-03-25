c3758be27a997e1ebcf8359bdfd0654f1fd575bc
jakkrish <jak.krish@gmail.com>
2025-03-25 09:06:49 +0530

    Styled Components added

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,5 +1,22 @@
+import styled from 'styled-components';
+
+const ButtonStyle = styled.button`
+  background-color: #fafafa;
+  color: #333;
+  border: 1px solid #ccc;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  cursor: pointer;
+  display: block;
+  margin: 0.5em;
+
+  &:hover {
+    background-color: #eee;
+  }
+`;
+
 const Button = ({onClick, children})=>{
-    return <div onClick={onClick}>{children}</div>
+    return <ButtonStyle onClick={onClick}>{children}</ButtonStyle>
 }
 
 export default Button
